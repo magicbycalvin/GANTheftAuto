026c65a8836f4dba2676fc9390fd401562a437c6
magicbycalvin <fake9475842@email.com>
2021-04-26 11:32:13 -0500

    Created initial directory architecture. Included several segmented images from CARLA recording.

---
+++ main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 26 11:27:09 2021
+
+@author: SysAdmin
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import trajgan
+
+
+if __name__ == '__main__':
+    pass
