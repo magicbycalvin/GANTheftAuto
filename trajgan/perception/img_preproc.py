a73955b81ffb66dee67c7ce0db19f955c9519dee
magicbycalvin <fake9475842@email.com>
2021-04-26 11:51:27 -0500

    Created the image preprocessing module.

---
+++ trajgan/perception/img_preproc.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 26 11:45:25 2021
+
+@author: SysAdmin
+"""
+
+import os
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class ImgPreprocessor:
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    img_path = '../../data/carla-recordings'
+    img_name = 'test_image221411.png'
